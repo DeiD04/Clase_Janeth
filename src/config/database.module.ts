62c8fa52bc55543d79744a67812da8d571fe7985
Jhonatan David Mosquera <jhonatan.mosquera@oncredit.com.co>
2025-03-20 16:24:13 -0500

    Type: fix connection mongosee in nestjs

--- src/config/database.module.ts
+++ src/config/database.module.ts
@@ -5,10 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`, {
-      user: process.env.MONGO_USER,
-      pass: process.env.MONGO_PASSWORD,
-      dbName: process.env.MONGO_DB,
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGO_URI,
+      }),
     })
   ],
 })
